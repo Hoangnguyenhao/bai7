a0f16455e142158b6ce70015533be5b886c26ccb
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-03-15 22:30:32 +0700

    Add files via upload

---
+++ Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai7.Properties.Models
+{
+    public abstract class Vehicle
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public abstract void DisplayInfo();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using bai7.Properties.Models.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai7
+{
+    class Program
+    {
+        static void Main()
+        {
+            CarManager manager = new CarManager();
+            while (true)
+            {
+                Console.WriteLine("\n1. Thêm xe mới");
+                Console.WriteLine("2. Hiển thị danh sách xe");
+                Console.WriteLine("3. Tìm kiếm xe theo tên");
+                Console.WriteLine("4. Xóa xe theo ID");
+                Console.WriteLine("5. Sắp xếp xe theo giá tăng dần");
+                Console.WriteLine("6. Chỉnh sửa thông tin xe");
+                Console.WriteLine("7. Thoát");
+                Console.Write("Chọn chức năng: ");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        manager.AddCar();
+                        break;
+                    case "2":
+                        manager.ShowCars();
+                        break;
+                    case "3":
+                        manager.SearchCarByName();
+                        break;
+                    case "4":
+                        manager.RemoveCarByID();
+                        break;
+                    case "5":
+                        manager.SortCarsByPrice();
+                        break;
+                    case "6":
+                        manager.EditCarByID();
+                        break;
+                    case "7":
+                        return;
+                    default:
+                        Console.WriteLine("Lựa chọn không hợp lệ! Hãy chọn lại.");
+                        break;
+                }
+            }
+        }
+    }
+}
